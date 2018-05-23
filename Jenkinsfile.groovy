d8ef9d1c721be0b39e8787378df650670fb61b27
Rob <brem_robert@hotmail.com>
2018-05-23 17:10:29 +0200

    initial commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,36 @@
+@Library('semantic_releasing') _
+
+podTemplate(label: 'mypod', containers: [
+        containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.0', command: 'cat', ttyEnabled: true)
+],
+        volumes: [
+                hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
+        ]) {
+    node('mypod') {
+        properties([
+                buildDiscarder(
+                        logRotator(artifactDaysToKeepStr: '',
+                                artifactNumToKeepStr: '',
+                                daysToKeepStr: '',
+                                numToKeepStr: '30'
+                        )
+                ),
+                pipelineTriggers([cron('30 1 * * *')])
+        ])
+
+        stage('create backup') {
+            currentBuild.displayName = getTimeDateDisplayName()
+
+            def kc = 'kubectl -n test'
+            def containerPath = '/opt/jboss/prometheus-2.0.0.linux-amd64/data'
+            def containerName = 'prometheus'
+            def podLabel = 'app=prometheus'
+            def repositoryUrl = 'bitbucket.org/khinkali/prometheus_backup'
+            container('kubectl') {
+                backup(podLabel, containerName, containerPath, repositoryUrl, kc)
+            }
+        }
+
+    }
+}
+
