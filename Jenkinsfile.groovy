753b9b092632a5ca7726304522461024f1fbfaab
Rob <brem_robert@hotmail.com>
2018-06-01 13:46:13 +0200

    [MONITOR] adding slack message on fail

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,31 +6,36 @@ podTemplate(label: 'mypod', containers: [
         volumes: [
                 hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
         ]) {
-    node('mypod') {
-        properties([
-                buildDiscarder(
-                        logRotator(artifactDaysToKeepStr: '',
-                                artifactNumToKeepStr: '',
-                                daysToKeepStr: '',
-                                numToKeepStr: '30'
-                        )
-                ),
-                pipelineTriggers([cron('30 1 * * *')])
-        ])
+    try {
+        node('mypod') {
+            properties([
+                    buildDiscarder(
+                            logRotator(artifactDaysToKeepStr: '',
+                                    artifactNumToKeepStr: '',
+                                    daysToKeepStr: '',
+                                    numToKeepStr: '30'
+                            )
+                    ),
+                    pipelineTriggers([cron('30 1 * * *')])
+            ])
 
-        stage('create backup') {
-            currentBuild.displayName = getTimeDateDisplayName()
+            stage('create backup') {
+                currentBuild.displayName = getTimeDateDisplayName()
 
-            def kc = 'kubectl -n test'
-            def containerPath = '/opt/jboss/prometheus-2.0.0.linux-amd64/data'
-            def containerName = 'prometheus'
-            def podLabel = 'app=prometheus'
-            def repositoryUrl = 'bitbucket.org/khinkali/prometheus_backup'
-            container('kubectl') {
-                backup(podLabel, containerName, containerPath, repositoryUrl, kc)
+                def kc = 'kubectl -n test'
+                def containerPath = '/opt/jboss/prometheus-2.0.0.linux-amd64/data'
+                def containerName = 'prometheus'
+                def podLabel = 'app=prometheus'
+                def repositoryUrl = 'bitbucket.org/khinkali/prometheus_backup'
+                container('kubectl') {
+                    backup(podLabel, containerName, containerPath, repositoryUrl, kc)
+                }
             }
+
         }
 
+    } catch (all) {
+        slackSend "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
     }
 }
 
